eb9212d2ef960997ac84d983b24f8dc8400de794
Rodney Lab <codesign@rodneylab.com>
2022-01-07 16:26:57 +0000

    ci: 🐝 initial commit

---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+use crate::telegram_client::TelegramClient;
+use serde::{Deserialize, Serialize};
+
+use worker::*;
+
+mod telegram_client;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    );
+}
+
+#[derive(Deserialize, Serialize)]
+struct MuxData {
+    status: String,
+    id: String,
+    duration: f32,
+    created_at: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct MuxEvent {
+    r#type: String,
+    data: MuxData,
+    created_at: String,
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
+    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
+    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
+    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
+    let router = Router::new();
+
+    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
+    // functionality and a `RouteContext` which you can use to  and get route parameters and
+    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    router
+        .post_async("/mux-endpoint", |mut req, ctx| async move {
+            let data: MuxEvent;
+            match req.json().await {
+                Ok(res) => data = res,
+                Err(error) => return Response::error(error.to_string(), 400),
+            }
+            let telegram_message = serde_json::to_string_pretty(&data).unwrap();
+
+            let telegram_bot_api_token = ctx.var("TELEGRAM_BOT_API_TOKEN")?.to_string();
+            let telegram_bot_chat_id = ctx.var("TELEGRAM_BOT_CHAT_ID")?.to_string();
+            let telegram_client =
+                TelegramClient::new(&telegram_bot_api_token, &telegram_bot_chat_id, None);
+            telegram_client.send_message(&telegram_message).await;
+            Response::ok("Received loud and clear!")
+        })
+        .run(req, env)
+        .await
+}
---
+++ src/telegram_client.rs
@@ -0,0 +1,110 @@
+extern crate worker;
+
+use std::collections::HashMap;
+use worker::console_log;
+
+pub struct TelegramClient {
+    base_url: String,
+    bot_api_token: String,
+    bot_chat_id: String,
+}
+
+impl TelegramClient {
+    pub fn new(bot_api_token: &str, bot_chat_id: &str, base_url: Option<&str>) -> TelegramClient {
+        let actual_base_url: &str = match base_url {
+            Some(value) => value,
+            None => "https://api.telegram.org/",
+        };
+        TelegramClient {
+            bot_api_token: bot_api_token.into(),
+            bot_chat_id: bot_chat_id.into(),
+            base_url: actual_base_url.into(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn base_url(&self) -> &str {
+        &self.base_url
+    }
+
+    #[allow(dead_code)]
+    pub fn bot_api_token(&self) -> &str {
+        &self.bot_api_token
+    }
+
+    #[allow(dead_code)]
+    pub fn bot_chat_id(&self) -> &str {
+        &self.bot_chat_id
+    }
+
+    pub async fn send_message(&self, message: &str) -> bool {
+        let client = reqwest::Client::new();
+        let mut map = HashMap::<&str, &str>::new();
+        map.insert("chat_id", self.bot_chat_id.as_str());
+        map.insert("text", message);
+        let url = format!("{}bot{}/sendMessage", self.base_url, self.bot_api_token);
+
+        match client.post(url).json(&map).send().await {
+            Ok(_) => true,
+            Err(_) => {
+                console_log!("Telegram API response error");
+                false
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::telegram_client::TelegramClient;
+
+    #[test]
+    pub fn test_new() {
+        let url = "https://example.com/";
+        let bot_api_token = "123456789:AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQq";
+        let bot_chat_id = "-123456789";
+        let client = TelegramClient::new(bot_api_token, bot_chat_id, Some(url));
+
+        assert_eq!(client.bot_api_token(), bot_api_token);
+        assert_eq!(client.bot_chat_id(), bot_chat_id);
+        assert_eq!(client.base_url(), url);
+
+        let client = TelegramClient::new(bot_api_token, bot_chat_id, None);
+        assert_eq!(client.bot_api_token(), bot_api_token);
+        assert_eq!(client.bot_chat_id(), bot_chat_id);
+        assert_eq!(client.base_url(), "https://api.telegram.org/");
+    }
+
+    #[tokio::test]
+    pub async fn test_send_message() {
+        use httptest::{
+            matchers::{all_of, eq, json_decoded, request},
+            responders::status_code,
+            Expectation, Server,
+        };
+        use serde_json::json;
+
+        let _ = pretty_env_logger::try_init();
+        let server = Server::run();
+
+        let message = "test message";
+        let telegram_bot_api_token = "123456789:AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQq";
+        let telegram_bot_chat_id = "-123456789";
+        let path = &format!("/bot{}/sendMessage", telegram_bot_api_token);
+
+        server.expect(
+            Expectation::matching(all_of![
+                request::method("POST"),
+                request::path(path.to_string()),
+                request::body(json_decoded(eq(json!(
+            {"chat_id": telegram_bot_chat_id, "text": message } )))),
+            ])
+            .times(1)
+            .respond_with(status_code(200)),
+        );
+        let url: &str = &server.url_str("");
+        let client = TelegramClient::new(telegram_bot_api_token, telegram_bot_chat_id, Some(url));
+        let result = client.send_message(&message).await;
+        assert!(result);
+    }
+}
